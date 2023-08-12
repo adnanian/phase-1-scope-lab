fd398cfca08a6d2ad923cc80ada80c9109fe6357
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-08-12 12:51:26 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+// Test 1
+var customerName = 'bob';
+
+
+// Test 2
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Test 3
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Test 4
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+// Test 5
+const leastFavoriteCustomer = 'un-bob';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'still bob';
+}
